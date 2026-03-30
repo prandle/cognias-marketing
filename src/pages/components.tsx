15d7a7f8c32f8d28e55082fd015c804df5320622
Paul Randle <mail@paulrandle.co.uk>
2026-03-30 16:13:57 +0100

    UI library

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -1,4 +1,4 @@
-import { Container } from "../components/layout/index";
+import { Container, FullWidthBand } from "../components/layout/index";
 import { Button } from "../components/ui/index";
 import { GetStarted } from "../components/widgets/index";
 
@@ -11,6 +11,7 @@ export default function ComponentsLibrary() {
     </Container>
 
     <Container>
+      <h2>Buttons Light Background</h2>
         <Button
           variant="primary"
           className="px-6 py-3">
@@ -38,6 +39,36 @@ export default function ComponentsLibrary() {
         </Button>
 
     </Container>
+    <FullWidthBand variant="primary">
+      <h2>Buttons Dark Background</h2>
+        <Button
+          variant="primary"
+          className="px-6 py-3">
+          Primary Button
+        </Button>
+        <Button
+          variant="secondary"
+          className="px-6 py-3">
+          Secondary Button
+        </Button>
+        <Button
+          variant="outline"
+          className="px-6 py-3">
+          Outline Button
+        </Button>
+        <Button
+          variant="ghost"
+          className="px-6 py-3">
+          Ghost Button
+        </Button>
+        <Button
+          variant="destructive"
+          className="px-6 py-3">
+          Destroy Button
+        </Button>
+
+    </FullWidthBand>
+
 </>
 
   )
